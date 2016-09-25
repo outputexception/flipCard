7c629044b18700e01338faf64f556330dab8cc66
outputexception <outputexception@gmail.com>
2016-09-25 05:18:12 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+'strict'
+
+// this.addEventListener('DOMContentLoaded', e => {
+//   var frontCard = document.getElementsByClassName('frontCard')[0]
+//   var backCard = document.getElementsByClassName('backCard')[0]
+//
+//   frontCard.addEventListener('click', ev => {
+//     ev.target.className = ev.target.className === 'frontCard' ? 'backCard' : 'frontCard'
+//   })
+//
+//   backCard.addEventListener('click', ev => {
+//     ev.target.className = ev.target.className === 'frontCard' ? 'backCard' : 'frontCard'
+//   })
+// })
+
+this.addEventListener('DOMContentLoaded', event => {
+  var flipCard = document.getElementsByClassName('flipCard')[0]
+  flipCard.addEventListener('click', e => {
+    flipCard.className = flipCard.className === 'flipCard' ? 'flipCardBack' : 'flipCard'
+  }, true)
+})
